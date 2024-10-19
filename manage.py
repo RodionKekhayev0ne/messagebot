f0f23750da375f0ea0bb9c33f4a061d3ea8d995f
Kekhayev Rodion <Kekhayev.r@mail.ru>
2024-10-19 12:54:03 +0500

    pre-prod v0.1

--- manage.py
+++ manage.py
@@ -2,11 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import threading
+
+
+def start_bot():
+    from botapp.bot import start_bot
+    import asyncio
+    asyncio.run(start_bot())
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messagebot.settings')
+
+    # Запускаем бота в фоновом потоке
+    bot_thread = threading.Thread(target=start_bot)
+    bot_thread.start()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +27,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
